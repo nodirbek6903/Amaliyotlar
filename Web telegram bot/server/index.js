8fa88cebc9d11a5c35ff41dcc46b2745e556cddb
nodirchik6903 <nu2913634@gmail.com>
2023-11-24 10:16:05 +0500

    inline button & express api

--- Web telegram bot/server/index.js
+++ Web telegram bot/server/index.js
@@ -89,7 +89,27 @@ const bootstrap = () => {
 bootstrap();
 
 app.post("/web-data",async (req,res) => {
-    
+    const {queryId, products} = req.body
+
+
+    try {
+       await bot.answerWebAppQuery(queryId, {
+        type: "article",
+        id: queryId,
+        title: "Muvaffaqqiyatli xarid qildingiz",
+        input_message_content: {
+            message_text: `Xaridingiz bilan tabriklayman, siz ${products.reduce((a,c) => a+c.price * c.quantity, 0)
+            .toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD"
+            })} qiymatga ega mahsulot sotib oldingiz, ${products.map(c => `${c.title} ${c.quantity}x`)
+        .join(", ")}`
+        }
+       }) 
+       return res.status(200).json({})
+    } catch (error) {
+        return res.status(500).json({})
+    }
 })
 
 app.listen(process.env.PORT || 8000,() => console.log("Server started"))
